ec5d597fc7f5e771d6864d5a98a349db13d2bffe
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-09-11 15:36:54 +0300

    database creation

--- migrations/2_create_table.up.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-CREATE TABLE installer (
-	name        varchar(60) NOT NULL PRIMARY KEY,
-	description text NOT NULL,
-	thumbnail	text NOT NULL,
-	provides	text[],
-	versions	text[] NOT NULL
-);
-END;
--- migrations/3_add_tsvector.down.sql
+++
@@ -1,4 +0,0 @@
-BEGIN;
-DROP INDEX ix_installer_tsv;
-ALTER TABLE installer DROP COLUMN tsv;
-END;
