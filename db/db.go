3faa7cc55c4f395a2cc93a2b12165f82bb714872
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-09-12 14:12:11 +0300

    removed the db setup and added a new full text search function

--- db/db.go
+++ db/db.go
@@ -14,20 +14,6 @@ import (
 var log = util.GetLogger()
 var config = util.GetConfig()
 
-var createDB = `
-DROP DATABASE IF EXISTS installers;
-CREATE DATABASE installers;
-`
-var createTable = `
-CREATE TABLE installer (
-	name        varchar(60) NOT NULL PRIMARY KEY,
-	description text NOT NULL,
-	thumbnail	text NOT NULL,
-	provides	text[],
-	versions	text[] NOT NULL
-);
-`
-
 func stripNilValues(in map[string]interface{}) map[string]interface{} {
 	out := make(map[string]interface{})
 	for k, v := range in {
@@ -56,24 +42,9 @@ func PGArrayToArray(pgarray string) []string {
 }
 
 func dbConnectionString() string {
-	return fmt.Sprintf("host=%s port=%d dbname=%s user=%s sslmode=disable", config.DBHost, config.DBPort, config.DBName, config.DBUser)
-}
-
-// SetupDB creates the db and table
-func SetupDB() {
-	log.Infof("Initializing database on endpoint %s", config.DBHost)
-	db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%d user=%s sslmode=disable", config.DBHost, config.DBPort, config.DBUser))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	db.MustExec(createDB)
-
-	db, err = sqlx.Connect("postgres", dbConnectionString())
-	if err != nil {
-		log.Fatalln(err)
-	}
-	db.MustExec(createTable)
-	log.Info("Database initialisation finished")
+	constr := fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=disable", config.DBHost, config.DBPort, config.DBName, config.DBUser, config.DBPass)
+	log.Debug(constr)
+	return constr
 }
 
 // Insert takes a db Installer and persists it to the database
@@ -148,18 +119,57 @@ func Get(name string) (Installer, bool) {
 	return installers[0], true
 }
 
-// Search searches installers based on the provides field
-func Search(providerType string) ([]Installer, error) {
+// SearchProvider searches installers based on the provides field
+func SearchProvider(providerType string) ([]Installer, error) {
 	db, err := sqlx.Connect("postgres", dbConnectionString())
 	if err != nil {
 		return nil, err
 	}
 
 	psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
-	sql, args, err := psql.Select("*").From("installer").Where("array_length(provides, 1) > 0 AND (?::string) = ANY(provides)", providerType).ToSql()
+	sql, args, err := psql.Select("name", "description", "thumbnail", "provides", "versions").From("installer").Where("array_length(provides, 1) > 0 AND (?) = ANY(provides)", providerType).ToSql()
 	if err != nil {
 		return nil, err
 	}
+
+	log.Debugf("Performing search query: {%s} using arguments {%v}", sql, args)
+	installers := []Installer{}
+	rows, err := db.Queryx(sql, args...)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var installer Installer
+		err := rows.Scan(&installer.Name, &installer.Description, &installer.Thumbnail, pq.Array(&installer.Provides), pq.Array(&installer.Versions))
+		if err != nil {
+			return nil, err
+		}
+		installers = append(installers, installer)
+	}
+
+	return installers, nil
+}
+
+// Search searches installers using a full text search on name, description and provides field
+func Search(searchTerm string) ([]Installer, error) {
+	db, err := sqlx.Connect("postgres", dbConnectionString())
+	if err != nil {
+		return nil, err
+	}
+
+	sql := `SELECT name, description, thumbnail, provides, versions
+FROM ( SELECT
+        name, description, thumbnail, provides, versions,
+        to_tsvector(name) ||
+        to_tsvector(description) ||
+        to_tsvector(array_to_string(provides, ' '))
+       AS document
+       FROM installer
+       GROUP BY name) installer_search
+WHERE installer_search.document @@ to_tsquery($1)
+ORDER BY ts_rank(installer_search.document, to_tsquery($1)) DESC;`
+
+	args := []interface{}{searchTerm}
 
 	log.Debugf("Performing search query: {%s} using arguments {%v}", sql, args)
 	installers := []Installer{}
