9ed8f203295872ccae890f6737efbdf2e27d38d6
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-09-12 13:53:26 +0300

    added the provides field to the tesvector weight. Will be used in the general search

--- migrations/2_add_tsvector.up.sql
+++ migrations/2_add_tsvector.up.sql
@@ -2,6 +2,7 @@ BEGIN;
 ALTER TABLE installer ADD COLUMN tsv tsvector;
 UPDATE installer SET tsv =
     setweight(to_tsvector(name), 'A') ||
-    setweight(to_tsvector(description), 'B');
+    setweight(to_tsvector(array_to_string(provides, ' ')), 'B') ||
+    setweight(to_tsvector(description), 'C');
 CREATE INDEX ix_installer_tsv ON installer USING GIN(tsv);
 END;
